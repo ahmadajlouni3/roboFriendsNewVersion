f1a0ed09f1a8cf81974030f2789ecdb2c47c16de
ahmadajlouni3 <bootsetup1@gmail.com>
2024-01-28 16:20:12 +0000

    update to hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,41 +1,70 @@
-import React , { Component } from 'react';
+import React , { useEffect, useState } from 'react';
 import Search from '../components/searchBox';
 import CardList  from '../components/cardList';
 import './App.css';
 
-class App extends Component{
-    constructor(){
-        super()
-        this.state= {
-            robots: [],
-            searchField: ''
-        }
-    }
+// class App extends Component{
+//     constructor(){
+//         super()
+//         this.state= {
+//             robots: [],
+//             searchField: ''
+//         }
+//     }
 
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
+//     componentDidMount(){
+//         fetch('https://jsonplaceholder.typicode.com/users')
+//         .then(res => res.json())
+//         .then(user => this.setState({ robots : user }))
+//     }
+
+//     onSearchChange = (event) => {
+//         this.setState({ searchField : event.target.value });
+//     }
+
+//     render() {
+//         const { robots , searchField } = this.state;
+//         const filtered = robots.filter(robot => {
+//             return robot.name.toLowerCase().includes(searchField.toLowerCase());
+//         })
+//         return !filtered ? 
+//         <h1>Loading</h1> : 
+//         (
+//             <div>
+//                 
+//             </div>
+//         )
+//     }
+// }
+
+
+
+const App = () =>{
+    const [robots , setRobots] = useState([]);
+    const [search , setSearch] = useState('');
+
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/users")
         .then(res => res.json())
-        .then(user => this.setState({ robots : user }))
-    }
+        .then(data => setRobots(data))
+    } , [setRobots])
 
-    onSearchChange = (event) => {
-        this.setState({ searchField : event.target.value });
-    }
+    const onSearchChange = (e) => setSearch(e.target.value)
 
-    render() {
-        const { robots , searchField } = this.state;
-        const filtered = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase());
-        })
-        return !filtered ? 
-        <h1>Loading</h1> : 
-        (
-            <div>
-                <h1>Cat Friends</h1>
-                <Search searchChange={this.onSearchChange}/>
-                <CardList cats={filtered}/>
-            </div>
+    const filtered = robots.filter(user => {
+        return( 
+        user.name.toLowerCase()
+        .includes(search.toLowerCase())
         )
-    }
+    })
+
+    return (
+        <div>
+          <h1>Cat Friends</h1>
+          <Search searchChange={onSearchChange}/>
+          <CardList cats={filtered}/>
+        </div>
+    )
 }
+
 export default App;
